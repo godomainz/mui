66368ed6462a8d2f478d8f93dbcc2c24d917a420
Akila Mendis <akila@oneit.com.au>
2022-01-25 14:42:59 +0530

    14. AppBar Transitions

--- src/components/UI/Header/Header.tsx
+++ src/components/UI/Header/Header.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import ElevationScroll from './ElevationScroll/ElevationScroll';
 
-const Header = () => {
+const Header = (props:any) => {
     return (
-        <AppBar position="fixed">
-            <Toolbar>
-                Arc Development
-            </Toolbar>
-        </AppBar>
+        <ElevationScroll {...props}>
+            <AppBar position="fixed">
+                <Toolbar>
+                    Arc Development
+                </Toolbar>
+            </AppBar>
+        </ElevationScroll>
+        
     );
 };
 export default Header;
