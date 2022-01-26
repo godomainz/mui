eed8016c5e134c85098fee238609571a35a858a6
Akila Mendis <akila@oneit.com.au>
2022-01-26 14:20:08 +0530

    20. Styling - Inline

--- src/components/UI/Header/Header.tsx
+++ src/components/UI/Header/Header.tsx
@@ -3,17 +3,21 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import ElevationScroll from '../ElevationScroll/ElevationScroll';
 import { Typography } from '@mui/material';
+import { useStyles } from './Header.styles';
 
 const Header = (props:any) => {
+    const classes = useStyles();
     return (
-        <ElevationScroll {...props}>
-            <AppBar position="fixed">
-                <Toolbar>
-                    <Typography variant="h3">Arc Development</Typography>
-                </Toolbar>
-            </AppBar>
-        </ElevationScroll>
-        
+        <React.Fragment>
+            <ElevationScroll {...props}>
+                <AppBar position="fixed">
+                    <Toolbar>
+                        <Typography variant="h3">Arc Development</Typography>
+                    </Toolbar>
+                </AppBar>
+            </ElevationScroll>
+            <div className={classes.toolBarMargin} />
+        </React.Fragment>
     );
 };
 export default Header;
